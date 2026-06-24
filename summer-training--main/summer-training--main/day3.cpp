e0f0596b17a5f16d2e5b6423119ebaa49dbd7f78
Yogesh703456 <yogesh703456@gmail.com>
2026-06-24 12:06:21 +0530

    update

--- summer-training--main/summer-training--main/day3.cpp
+++ summer-training--main/summer-training--main/day3.cpp
@@ -1,7 +1,3 @@
-Day 3 Questions ....
-
-
-
 // program 1: print 1 to n.
 
 
@@ -21,109 +17,109 @@ int main(){
 
 // program 2....sum of n number.
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n ;
-    int sum=0;
-    cout<<"Enter the number : ";
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        sum=sum+i;
-    }
-    cout<<"Total sum : "<<sum;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n ;
+//     int sum=0;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     cout<<"Total sum : "<<sum;
 
-}
-
-
-
-// program 3....factorial.
-
-
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    int fac=1;
-    cout<<"Enter the number : ";
-    cin>>n;
-    for(int i=n;i>=1;i--){
-        fac=fac*i;
-    }
-    cout<<"The factorial : "<<fac;
-
-}
+// }
 
 
 
-// program 4....reverse number.
+// // program 3....factorial.
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int fac=1;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     for(int i=n;i>=1;i--){
+//         fac=fac*i;
+//     }
+//     cout<<"The factorial : "<<fac;
+
+// }
 
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
+// // program 4....reverse number.
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
     
-    int digit;
-    cout<<"Enter the number : ";
-    cin>>n;
+//     int digit;
+//     cout<<"Enter the number : ";
+//     cin>>n;
     
-    int rev=0;
-    for(int i=n;i>0;){
-        digit=n%10;
-        rev=rev*10+digit;
-        n=n/10;
-    }
-    cout<<rev;
-}
+//     int rev=0;
+//     for(int i=n;i>0;){
+//         digit=n%10;
+//         rev=rev*10+digit;
+//         n=n/10;
+//     }
+//     cout<<rev;
+// }
     
 
 
 
-// program 5....count digit.
+// // program 5....count digit.
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    int count=0;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int count=0;
    
-    cout<<"Enter the number : ";
-    cin>>n;
-    for(int i=n;i>0;){
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     for(int i=n;i>0;){
        
-        count++;
-        n=n/10;
-    }
-    cout<<count;
+//         count++;
+//         n=n/10;
+//     }
+//     cout<<count;
 
 
-}
+// }
 
 
 
-// program 6....find prime number.
+// // program 6....find prime number.
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the number : ";
-    cin>>n;
-    int count=0;
-    for(int i=1;i<=n;i++){
-        if(n%i==0){
-            count++;
-        }
-    }
-    if(count==2){
-        cout<<"It is a prime number";
-    }
-    else{
-        cout<<"It is not a prime number ";
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     int count=0;
+//     for(int i=1;i<=n;i++){
+//         if(n%i==0){
+//             count++;
+//         }
+//     }
+//     if(count==2){
+//         cout<<"It is a prime number";
+//     }
+//     else{
+//         cout<<"It is not a prime number ";
+//     }
 
-}
+// }
