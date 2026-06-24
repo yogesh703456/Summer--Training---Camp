d4cf69929c4c0fee115dd0c319a521c540ce4f1f
Yogesh703456 <yogesh703456@gmail.com>
2026-06-24 11:57:01 +0530

    update

---
+++ summer-training--main/summer-training--main/tempCodeRunnerFile.cpp
@@ -0,0 +1,147 @@
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"---->>>>Largest of three number<<<<----\n";
+//     int a,b,c;
+//     cout<<"Enter the first number : ";
+//     cin>>a;
+//     cout<<"Enter the second number : ";
+//     cin>>b;
+//     cout<<"Enter the third number : ";
+//     cin>>c;
+//     if(a>b & a>c){
+//         cout<<a<<" Is largest number";
+//     }
+//     else if(b>a & b>c){
+//         cout<<b<<" Is largest number";
+
+//     }
+//     else if(c>a & c>b){
+//         cout<<c<<" Is largest number";
+//     }
+// }
+
+
+
+// // program 3: leap year
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"---->>>>Leap year<<<<----\n";
+//     int year;
+//     cout<<"Enter the year : ";
+//     cin>>year;
+//     if(year%400==0){
+//         cout<<"It is a leap year";
+//     }
+//     else if(year%100==0 ){
+//         cout<<"IT is not a leap year ";
+//     }
+//     else if(year%4==0){
+//         cout<<"It is a leap year";
+//     }
+//     else{
+//         cout<<"It is not a leap year";
+//     }
+    
+//     }
+
+
+
+// // program 4: Grade calculator.
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the percentage : ";
+//     cin>>n;
+//     if(n>=90 && n<=100){
+//          cout<<"A+";
+//     }
+//     else if(n>=80 && n<90){
+//         cout<<"A";
+//     }
+//     else if(n>=70 && n<80){
+//         cout<<"B";
+//     }
+//     else if(n>=60 && n<70){
+//         cout<<"c";
+//     }
+//     else{
+//         cout<<"F";
+//     }
+
+// }
+
+
+
+// // program 5: positive /negative/zero.
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     if(n>0){
+//         cout<<"Positive";
+//     }
+//     else if(n<0){
+//         cout<<"Negative";
+//     }
+//     else{
+//         cout<<"Zero";
+//     }
+// }
+
+
+
+// // program 6: simple calculator.
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Enter the sign + - / * \n";
+//     int a,b,c;
+//     char n;
+    
+//     cout<<"Enter the first number : ";
+    
+//     cin>>a;
+//     cout<<"Enter the second number : ";
+//     cin>>b;
+//     cout<<"Enter the sign : ";
+//     cin>>n;
+    
+//     if(n=='+'){
+//         c=a+b;
+//         cout<<c;
+        
+//     }
+//     else if(n=='-'){
+//         c=a-b;
+//         cout<<c;
+//     }
+//     else if(n=='*'){
+//         c=a*b;
+//         cout<<c;
+//     }
+//     else if(n=='/'){
+//         c=a/b;
+//         cout<<c;
+//     }
+//     else{
+//         cout<<"error,Enter the valid sign";
+//     }
+    
+// }
