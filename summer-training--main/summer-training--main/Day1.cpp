32f6eb2dd5e161dd3dbea3c631955c36c92585bf
Yogesh703456 <yogesh703456@gmail.com>
2026-06-24 10:48:45 +0530

    update

---
+++ summer-training--main/summer-training--main/Day1.cpp
@@ -0,0 +1,110 @@
+
+
+
+
+//program 1:hello world.
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Helo world";
+}
+
+
+// program2: sum of two number.answer :
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,n1,sum;
+//     cout<<"Enter the first number : ";
+//     cin>>n;
+//     cout<<"Enter the second number : ";
+//     cin>>n1;
+//     sum=n+n1;
+//     cout<<"The sum of Two number : "<<sum;
+    
+// }
+
+
+
+// program 3: area of rectangle
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int l,b,rec;
+//     cout<<"The area of rectangle";
+//     cout<<"Enter the length : ";
+//     cin>>l;
+//     cout<<"Enter the breadth : ";
+//     cin>>b;
+
+//     rec=l*b;
+//     cout<<rec;
+
+// }
+
+
+
+// // program 4: simple interest.
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Simple interset\n";
+//     int si,p,r,t;
+//     cout<<"Enter the p : ";
+//     cin>>p;
+//     cout<<"Enter the r : ";
+//     cin>>r;
+//     cout<<"Enter the t : ";
+//     cin>>t;
+//     si=(p*r*t)/100;
+//     cout<<si;
+// }
+
+
+
+// // program 5: swap two number.
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int temp,a,b;
+//     cout<<"---->>>> Swap two number <<<<----\n";
+//     cout<<"Enter the number : ";
+//     cin>>a;
+//     cout<<"Enter the number : ";
+//     cin>>b;
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     cout<<a<<endl;
+//     cout<<b;
+
+// }
+
+
+// program 6:  Celsius to Fahrenheit
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"---->>>> celsius to fahrenheit <<<<----\n";
+//     int f,c;
+//     cout<<"Enter the celsius : ";
+//     cin>>c;
+//     f=(c*9/5)+32;
+//     cout<<f;
+
+
+// }
+
+
+
